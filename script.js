0acbf1cc4988f8bd7b96316a73f543d974b47c6a
Sodaba Bashiri <sodababashiri@gmail.com>
2020-04-08 02:07:07 +0200

    login button redirects to the homepage

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 document.addEventListener("DOMContentLoaded", function(event) {
 
+    let users = [
+        {email:"sodababashiri@gmail.com", password:"aaa"},
+        {email:"rahimalizada@gmail.com", password:"bbb"},
+        {email:"sumanthini@gmail.com", password:"ccc"},
+        {email:"helen@gmail.com", password:"ddd"},
+        {email:"chayma.besuda@gmail.com", password:"eee"},
+    ];
+
     document.getElementById("btn").addEventListener("click", function(event) {
         let email = document.getElementById('email');
         let psw = document.getElementById('password');
@@ -7,10 +15,23 @@ document.addEventListener("DOMContentLoaded", function(event) {
         if (email.value.trim().length < 1 || psw.value.trim().length < 1 ) {
             alert('Please fill the field!');
         }
-        console.log(email.value + ',' + psw.value);
-        createTableElement(email.value,psw.value);
+        
+        for (let user = 0; user < users.length; user++) {
+            if (users[user].email == email.value && users[user].password == psw.value) {
+                let homepage = "file:///C:/Users/Powercoders/Desktop/Code/Js%20projects/simple-login/homepage.html";
+                window.location.replace(homepage);  
+                // createTableElement(email.value,psw.value);
+            } else {
+                let error = document.getElementById('userError');
+                error.style.display = 'block';
+            }
+        }
+
+        // createTableElement(email.value,psw.value);
         reset(email, psw);
     });
+
+    
 
     function createTableElement(email, password) {
         const tbody = document.getElementById('tbody');
